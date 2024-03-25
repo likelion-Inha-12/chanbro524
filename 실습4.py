010fda365dc52dae1650c8a354befa5c14c0b8ed
lee chanhyeong <12202049@inha.edu>
2024-03-25 20:28:33 +0900

    과제끝

---
+++ 실습4.py
@@ -0,0 +1,5 @@
+lst=['염종섭','정보통신공학과',25,'estp']
+lst2=['오다솔','컴퓨터공학과',23,'esfp']
+lst3=['김시원','산업경영공학과',23,'isfp']
+dic={lst[0]:[lst[1],lst[2],lst[3]],lst2[0]:[lst2[1],lst2[2],lst2[3]],lst3[0]:[lst3[1],lst3[2],lst3[3]]}
+print(dic)
