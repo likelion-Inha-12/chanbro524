ae4d644668d74b240864c9a78bcaf74a2c8be12d
chanbro0524 <12202049@inha.edu>
2024-04-07 23:30:17 +0900

    과제 제출합니다

--- config/seminar_project/seminar_project/urls.py
+++ config/seminar_project/seminar_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', include('util.urls')),
+    path('lion/', include('lionapp.urls')),
 ]
