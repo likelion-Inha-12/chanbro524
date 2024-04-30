b38e827df68233710b4fa451bfc05dd3621f7f6e
chanbro0524 <12202049@inha.edu>
2024-05-01 01:47:08 +0900

    4-10subject clear

--- config/seminar_project/lionapp/admin.py
+++ config/seminar_project/lionapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
-
+from .models import Comment
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
